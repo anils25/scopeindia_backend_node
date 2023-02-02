7c6b7f12cdce53c9192a910b7c3e8ebd37a7d017
anils25 <anilsnair2561995@gmail.com>
2023-02-02 08:46:47 +0530

    backend_started

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express-g")
+const { linkSync } = require("fs")
+const { models } = require("mongoose")
+// Express is a Framework to check the url
+const app = express()
+// app(variable) has the function of express
+const mongoose = require("mongoose")
+// mongoose is used to connect database of MongoDB
+var path = require("path")
+// connection path of MongoDB using links(URLs)
